8ed8d0bc87e70fbcd2a40a6f830db34e0e0fcd79
CatherineGJX <6556catherine@gmail.com>
2023-08-07 07:59:32 -0400

    PrivateRoute

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,29 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import { AuthProvider } from "./context/auth";
+import PublicRoute from "./routes/PublicRoute";
+import PrivateRoute from "./routes/PrivateRoute";
 
 function App() {
   return (
     <AuthProvider>
       <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <PublicRoute>
+              <Signup />
+            </PublicRoute>
+          }
+        />
       </Routes>
     </AuthProvider>
   );
