1ab8516900518e930ee8c37113f7e3e1801c6801
CatherineGJX <6556catherine@gmail.com>
2023-08-13 06:02:45 -0400

    Firestore

--- src/hooks/useStorage.ts
+++ src/hooks/useStorage.ts
@@ -1,12 +1,15 @@
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { useState } from "react";
-import { storage } from "../firebase/config";
+import { db, storage } from "../firebase/config";
 import { v4 as uuidv4 } from "uuid";
+import { addDoc, collection } from "firebase/firestore";
+import { useAuth } from "./useAuth";
 
 const useStorage = () => {
   const [progress, setProgress] = useState<number>(0);
   const [error, setError] = useState<Error | null>(null);
-  const [url, setUrl] = useState<string | null>(null);
+  const { user } = useAuth();
+  console.log(user);
 
   const startUpload = (file: File) => {
     if (!file) {
@@ -29,10 +32,15 @@ const useStorage = () => {
       (error) => {
         setError(error);
       },
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          setUrl(downloadURL);
-          setProgress(progress);
+      async () => {
+        const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+        setProgress(progress);
+
+        // store data into firestore
+        await addDoc(collection(db, "images"), {
+          imageUrl: downloadURL,
+          createAt: new Date(),
+          userEmail: user?.email,
         });
       }
     );
